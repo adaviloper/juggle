b77edffc1e0e57a1956a97b51e3e7124cb4b7d4c
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-04-26 08:12:04 -0500

    adding vue to default to typescript

--- lua/juggle_filetypes/init.lua
+++ lua/juggle_filetypes/init.lua
@@ -15,7 +15,7 @@ local function get_lang_from_buf(buf)
       }) or ""
    end
 
-   if ft == 'javascript' then
+   if vim.tbl_contains({ 'vue', 'javascript' }, ft) then
       return 'typescript'
    end
 
